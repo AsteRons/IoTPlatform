5ab6478d3e593a78ddd905e6bdfd5a98b5a5866c
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-27 11:10:15 +0200

    Initial server

---
+++ src/main/java/com/pw/saif/iotplatform/server/iotplatform/IoTPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.pw.saif.iotplatform.server.iotplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pw/saif/iotplatform/server/iotplatform/IoTPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.saif.iotplatform.server.iotplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
