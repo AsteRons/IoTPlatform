ccfd9e68f96b5f063c16767aa38f6c1e60681cbb
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-30 20:20:23 +0200

    add relation Sensor and SensorData

--- src/main/java/com/pw/saif/iotplatform/server/iotplatform/SensorsData/model/Sensor.java
+++ src/main/java/com/pw/saif/iotplatform/server/iotplatform/SensorsData/model/Sensor.java
@@ -1,4 +1,31 @@
 package com.pw.saif.iotplatform.server.iotplatform.SensorsData.model;
 
+import com.pw.saif.iotplatform.server.iotplatform.Users.model.User;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="sensor")
 public class Sensor {
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="importance")
+    public String importance;
+
+    @Column(name="url")
+    public String url;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "sensor")
+    private Set<SensorData> sensorData = new HashSet<>();
 }
