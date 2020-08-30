eba60eedbebcf53f33297512df2c2f3f68caeec0
Maciek Dom <maciej.rudnik2@gmail.com>
2020-08-30 20:20:08 +0200

    add user model

--- src/main/java/com/pw/saif/iotplatform/server/iotplatform/Users/model/User.java
+++ src/main/java/com/pw/saif/iotplatform/server/iotplatform/Users/model/User.java
@@ -1,15 +1,23 @@
 package com.pw.saif.iotplatform.server.iotplatform.Users.model;
 
 
-import javax.persistence.*;
+import com.pw.saif.iotplatform.server.iotplatform.SensorsData.model.Sensor;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name="userData")
+@Table(name="user")
 public class User {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id")
     private int id;
 
     @Column(name="firstName")
@@ -26,80 +34,6 @@ public class User {
 
     @Column(name="permissions")
     private String permissions;
-
-    // define constructors
-
-    public User(){
-
-    }
-
-    public User(int id,String firstName, String lastName, String login, String password, String permissions) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.login = login;
-        this.password = password;
-        this.permissions = permissions;
-
-    }
-
-    public User(String firstName, String lastName, String login, String password, String permissions) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.login = login;
-        this.password = password;
-        this.permissions = permissions;
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPermissions() {
-        return permissions;
-    }
-
-    public void setPermissions(String permissions) {
-        this.permissions = permissions;
-    }
-
 
 
     @Override
